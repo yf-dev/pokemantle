dc7692f7c9ecea8a745448cd95c0075db60423b7
Nesswit <rishubil@gmail.com>
2022-04-30 16:32:05 +0900

    Add copyright text

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -10,6 +10,13 @@
     </ClientOnly>
     <Faq class="mt-8" />
   </div>
+  <div class="px-2 mb-10 text-xs text-gray-700 text-center">
+    Pokémon © 2002-{{ new Date().getFullYear() }} Pokémon. © 1995-{{ new Date().getFullYear() }} Nintendo/Creatures
+    Inc./GAME FREAK inc. TM, ® and
+    Pokémon character names
+    are trademarks of Nintendo.<br>
+    No copyright or trademark infringement is intended in using Pokémon content on Pokémantle.
+  </div>
 </template>
 
 <script setup lang="ts">
